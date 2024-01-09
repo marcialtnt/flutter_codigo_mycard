7cc7a7ba0c9c433fbb340f5776432ee07d60f632
Marcial Ascencio <marcialtnt@gmail.com>
2024-01-08 22:43:32 -0500

    Todo junto

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,148 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: "MyCard App",
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Colors.indigo,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: 60.0,
+            backgroundColor: Colors.pinkAccent,
+            /*backgroundImage: NetworkImage(
+              "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg",
+            ),*/
+            backgroundImage: AssetImage("assets/images/persona1.jpeg"),
+          ),
+          SizedBox(
+            height: 12.0,
+          ),
+          Text(
+            "Fiorella Guadalupe Arias",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 26.0,
+              fontFamily: "Lobster",
+              fontWeight: FontWeight.w100,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          Text(
+            "FLUTTER DEVELOPER",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+              fontFamily: 'Manrope',
+              fontWeight: FontWeight.w300,
             ),
-          ],
-        ),
+          ),
+          SizedBox(
+            height: 3.0,
+          ),
+          SizedBox(
+            width: 180.0,
+            child: Divider(
+              thickness: 0.8,
+              color: Colors.white,
+            ),
+          ),
+          Card(
+            color: Colors.white,
+            elevation: 20,
+            margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: ListTile(
+              title: Text(
+                "+51 456 387 899",
+                style: TextStyle(
+                  fontFamily: "Manrope",
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              subtitle: Text(
+                "Teléfono",
+                style: TextStyle(
+                  fontFamily: "Manrope",
+                ),
+              ),
+              leading: Icon(
+                Icons.phone,
+                size: 30.0,
+                color: Colors.indigo,
+              ),
+            ),
+          ),
+          Card(
+            elevation: 10,
+            margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: ListTile(
+              title: Text(
+                "guadalupe_fiorella@gmail.com",
+                style: TextStyle(
+                  fontFamily: "Manrope",
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              subtitle: Text(
+                "Correo electrónico",
+                style: TextStyle(
+                  fontFamily: "Manrope",
+                ),
+              ),
+              leading: Icon(
+                Icons.email,
+                size: 30.0,
+                color: Colors.indigo,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 24.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/images/facebook.png",
+                height: 60.0,
+              ),
+              SizedBox(
+                width: 32.0,
+              ),
+              Image.asset(
+                "assets/images/twitter.png",
+                height: 60.0,
+              ),
+              SizedBox(
+                width: 32.0,
+              ),
+              Image.asset(
+                "assets/images/instagram.png",
+                height: 60.0,
+              ),
+            ],
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
